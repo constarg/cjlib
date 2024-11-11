208ce3b7d238c834f36319e5cce997f4d9b8f16c
constarg <constarg@pm.me>
2024-11-11 17:42:51 +0200

    Trying to figure out a bug

--- src/include/cjlib_dictionary.h
+++ src/include/cjlib_dictionary.h
@@ -1,7 +1,6 @@
-// @file: cjlib_dictionary.h
-
 /* File: cjlib_dictionary.h Constantinos Argyriou
  *
+ ************************************************************************
  * Copyright (C) 2024 Constantinos Argyriou
  *
  * This program is free software: you can redistribute it and/or modify
@@ -16,6 +15,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *************************************************************************
  */
 
 #ifndef CJLIB_DICTIONARY_H
