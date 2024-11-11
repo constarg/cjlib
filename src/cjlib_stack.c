3d50049dc2b7c8287f635f3101176b20762e422c
constarg <constarg@pm.me>
2024-11-11 17:33:16 +0200

    edit Trying to figure out a bug

--- src/cjlib_stack.c
+++ src/cjlib_stack.c
@@ -1,5 +1,5 @@
 /* File: cjlib_stack.c
- *
+ ************************************************************************
  * Copyright (C) 2024 Constantinos Argyriou
  *
  * This program is free software: you can redistribute it and/or modify
@@ -14,6 +14,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *************************************************************************
  */
 
 #include <stdbool.h>
--- src/include/cjlib_stack.h
+++ src/include/cjlib_stack.h
@@ -1,5 +1,6 @@
 /* File: cjlib_stack.h
-*
+ *
+ ************************************************************************
  * Copyright (C) 2024 Constantinos Argyriou
  *
  * This program is free software: you can redistribute it and/or modify
@@ -14,6 +15,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *************************************************************************
  */
 
 #ifndef CJLIB_STACK_H
